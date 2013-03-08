f4d7be0c47af43e1fbcdcaf5e6b16043bf7c69cd
Archibald Lu <chienius@outlook.com>
2013-03-08 17:34:15 +0800

    Update log

--- application/views/3_5_container_updatelog.php
+++ application/views/3_5_container_updatelog.php
@@ -5,6 +5,7 @@
 	</div>
 	<div id="content">
 		<ul class="items" style="font-size: 15px; line-height: 16px">
+			<li><b>2013年3月8日</b><p>修复时区及日期判断问题</p></li>
 			<li><b>2013年3月4日</b><p>修复作业字体显示大小不一的问题</p><p>修改“登录平台”菜单项的提示</p></li>
 			<li><b>2013年2月12日</b><p>日期前后浏览功能的改进</p><p>修复Legacy Browser提示的字体显示问题</p></li>
 			<li><b>2013年2月10日</b><p>添加Legacy Browser提示功能</p><p>优化IE6兼容性</p><p>修改日期显示背景</p><p>优化“转到今日”功能</p></li>
