71d54e26f228510998350b62a67c61e9690447cf
chienius <chienius@outlook.com>
2013-03-23 12:53:47 +0800

    Update Log

--- application/views/3_5_container_updatelog.php
+++ application/views/3_5_container_updatelog.php
@@ -4,7 +4,9 @@
 		更新记录
 	</div>
 	<div id="content">
+		<p>了解更详细的更新记录，请访问 <a href="https://github.com/chienius/Plain-Homework/commits/master" target="_blank">GitHub</a></p>
 		<ul class="items" style="font-size: 15px; line-height: 16px">
+			<li><b>2013年3月23日</b><p>修改作业项目正则过滤，修复作业吞字问题</p><p>修改登陆页面的部分用语</p></li>
 			<li><b>2013年3月8日</b><p>修复时区及日期判断问题</p></li>
 			<li><b>2013年3月4日</b><p>修复作业字体显示大小不一的问题</p><p>修改“登录平台”菜单项的提示</p></li>
 			<li><b>2013年2月12日</b><p>日期前后浏览功能的改进</p><p>修复Legacy Browser提示的字体显示问题</p></li>
