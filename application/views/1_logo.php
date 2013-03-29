4d2a25d430fb2ddc4ed9389faeb4868df64c4e1c
Archibald Lu <chienius@outlook.com>
2013-03-29 16:10:08 +0800

    添加“测试版”Logo标签

--- application/views/1_logo.php
+++ application/views/1_logo.php
@@ -3,6 +3,7 @@
 <div id="logo" onclick="window.location.href='<?=site_url() ?>'">
   <div id="en_logo">
     Plain<br />Homework
+    <div style="font-size: small; color: red;">测试版</div>
   </div>
   <div id="zh_logo">
     干净看作业
