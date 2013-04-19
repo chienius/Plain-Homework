ede3696174d31138257e9007af2264d6afc41951
Archibald Lu <chienius@outlook.com>
2013-04-19 16:56:09 +0800

    JS的RWD简单支持

--- assets/common.js
+++ assets/common.js
@@ -1,52 +1,59 @@
-// $(document).ready(function(e) {
-	// $('a').tooltip({position: {
-		// my: "left+15 center", at: "right center"
-	// }});
-	// $('.arrow').tooltip();
-	// $('.popup').dialog({
-		// autoOpen: false,
-		// modal: true,
-		// resizable: false,
-		// draggable: false,
-	// });
-	// $('#datepicker').datepicker();
-	// $('#sidebar').delay(500).fadeOut();
-// 	
-	// $('#sidebar_wrapper').hover(function(){
-		// $(this).find('#sidebar').stop().fadeIn();
-	// }, function(){
-		// $(this).find('#sidebar').stop().fadeOut();
-	// });
-	// $('#date_wrapper').hover(function(){
-		// $('#picker, #date .arrow').stop().fadeIn();
-	// }, function(){
-		// $('#picker, #date .arrow').stop().fadeOut();
-	// });
-	// $('#date .arrow a').hover(function(){
-		// $(this).css('color', '#000');
-	// }, function(){
-		// $(this).css('color', '#c1c1c1');
-	// });
-// });
-// 
-// var HC_MODE=0;
-// function highContract(){
-	// switch(HC_MODE){
-		// case 0:
-			// HC_MODE=1;
-			// $('.screen_hover').css('background-color', '#000');
-			// break;
-		// case 1:
-			// $('.screen_hover').css('background-color', '#fff');
-			// HC_MODE=0;
-	// };
-	// $('.screen_hover').fadeIn().queue(function(){
-		// $('.subject').toggleClass('hsubject');
-		// $('.items').toggleClass('hitems');
-		// $('#sidebar').toggleClass('hsidebar')
-		// $('body').toggleClass('hbody');
-		// $('#copyinfo, #date').toggle();
-		// $(this).dequeue();
-	// });
-	// $('.screen_hover').delay(500).fadeOut();
-// }
+$(document).ready(function(e) {
+	$('a').tooltip({position: {
+		my: "left+15 center", at: "right center"
+	}});
+	$('.arrow').tooltip();
+	$('.popup').dialog({
+		autoOpen: false,
+		modal: true,
+		resizable: false,
+		draggable: false,
+	});
+	$('#datepicker').datepicker();
+	
+	if($(window).width()>800){
+		$('#sidebar').delay(500).fadeOut();	
+		$('#sidebar_wrapper').hover(function(){
+			$(this).find('#sidebar').stop().fadeIn();
+		}, function(){
+			$(this).find('#sidebar').stop().fadeOut();
+		});
+		$('#date_wrapper').hover(function(){
+			$('#picker, #date .arrow').stop().fadeIn();
+		}, function(){
+			$('#picker, #date .arrow').stop().fadeOut();
+		});
+		$('#date .arrow a').hover(function(){
+			$(this).css('color', '#000');
+		}, function(){
+			$(this).css('color', '#c1c1c1');
+		});
+	}else{
+		$('#sidebar').click(function(){
+			$('#sidebar #menu').toggle();
+		});
+	};
+	
+});
+
+var HC_MODE=0;
+function highContract(){
+	switch(HC_MODE){
+		case 0:
+			HC_MODE=1;
+			$('.screen_hover').css('background-color', '#000');
+			break;
+		case 1:
+			$('.screen_hover').css('background-color', '#fff');
+			HC_MODE=0;
+	};
+	$('.screen_hover').fadeIn().queue(function(){
+		$('.subject').toggleClass('hsubject');
+		$('.items').toggleClass('hitems');
+		$('#sidebar').toggleClass('hsidebar')
+		$('body').toggleClass('hbody');
+		$('#copyinfo, #date').toggle();
+		$(this).dequeue();
+	});
+	$('.screen_hover').delay(500).fadeOut();
+}
